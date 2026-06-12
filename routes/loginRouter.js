1ee30902db6846a3a69d12efcb87fff579b221f7
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-12 07:08:15 +0100

    feat: add passportJS authenticate to login router
    Use PassportJS LocalStrategy to authenticate and reroute users on
    logging in

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { loginPage } from "../controllers/loginController.js";
+import passport from "passport";
 
 const loginRouter = Router();
 
+loginRouter.post(
+  "/",
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 loginRouter.get("/", loginPage);
 
 export { loginRouter };
