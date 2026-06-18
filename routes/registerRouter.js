096d3a8ca9779a9882f83d52d88e53d7c3bdf5f1
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-18 13:51:49 +0100

    feat: add user registration handler controllers
    Add routes for checking email address and username uniqueness
    Add routes for registering a user

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -1,8 +1,25 @@
 import { Router } from "express";
-import { registerGet } from "../controllers/registerController.js";
+import {
+  buildUsernameIfMissing,
+  checkEmailUnique,
+  checkUsernameUnique,
+  handleValidationErrors,
+  registerGet,
+  registerPost,
+} from "../controllers/registerController.js";
+import { registerValidationRules } from "../validators/registerValidation.js";
 
 const registerRouter = Router();
 
+registerRouter.get("/check-email", checkEmailUnique);
+registerRouter.get("/check-username", checkUsernameUnique);
+registerRouter.post(
+  "/",
+  buildUsernameIfMissing,
+  registerValidationRules,
+  handleValidationErrors,
+  registerPost
+);
 registerRouter.get("/", registerGet);
 
 export { registerRouter };
