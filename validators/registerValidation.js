ff1bc763601145e869ec9bd1a8fbc2a4ac5490b6
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-18 13:44:43 +0100

    feat: create registerPost validation rules file
    Add validation rules meant for validating register POST request
    form data

---
+++ validators/registerValidation.js
@@ -0,0 +1,23 @@
+import { body } from "express-validator";
+
+const registerValidationRules = [
+  body("email")
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("Please enter a valid email address"),
+  body("username").notEmpty().trim().withMessage("Username is required"),
+  body("password")
+    .notEmpty()
+    .trim()
+    .withMessage("Password is required")
+    .isStrongPassword({
+      minLength: 8,
+      minUppercase: 1,
+      minLowercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+    .withMessage("Enter a stronger password."),
+];
+
+export { registerValidationRules };
