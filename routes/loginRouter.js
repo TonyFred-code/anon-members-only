145544756ad0299e71085664989fb5ff11da84fe
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-19 17:52:05 +0100

    refactor: use `userLoginAuth` middleware for auth
    Use `userLoginAuth` middleware to authenticate users before processing
    demo account details

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
-import { loginPage } from "../controllers/loginController.js";
+import { loginPage, userLoginAuth } from "../controllers/loginController.js";
 import passport from "passport";
 
 const loginRouter = Router();
 
-loginRouter.post(
-  "/",
-  passport.authenticate("local", {
-    successRedirect: "/home",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
-);
+loginRouter.post("/", userLoginAuth);
 loginRouter.get("/", loginPage);
 
 export { loginRouter };
