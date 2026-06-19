69ac6f9f39a93eed17457cad536c6ba5df5d63b4
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-19 17:56:07 +0100

    feat: create login page script file for auth
    Create script to handle processing server requests and serving errors
    as appropriate

---
+++ public/pages/login/main.js
@@ -0,0 +1,115 @@
+const emailInput = document.querySelector("input#email");
+const passwordInput = document.querySelector("input#password");
+const submitBtn = document.querySelector("button#submit");
+
+const fieldErrVisible = {
+  email: false,
+  network: false,
+  password: false,
+};
+
+function showFieldError(field, errMessage) {
+  const element = document.querySelector(`[data-error='${field}']`);
+
+  if (!element) return;
+  element.textContent = errMessage;
+  element.classList.remove("hidden");
+  fieldErrVisible[field] = true;
+}
+
+function hideFieldError(field) {
+  const element = document.querySelector(`[data-error='${field}']`);
+
+  if (!element) return;
+
+  element.classList.add("hidden");
+  fieldErrVisible[field] = false;
+}
+
+function isEmailAddressValid(emailAddress) {
+  return /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i.test(emailAddress.trim());
+}
+
+emailInput.addEventListener("focus", () => {
+  if (fieldErrVisible.email) {
+    hideFieldError("email");
+  }
+});
+
+emailInput.addEventListener("input", () => {
+  if (fieldErrVisible.email) hideFieldError("email");
+
+  if (fieldErrVisible.network) hideFieldError("network");
+});
+
+emailInput.addEventListener("blur", () => {
+  if (
+    !emailInput.value ||
+    (emailInput.value && !isEmailAddressValid(emailInput.value))
+  ) {
+    showFieldError("email", "Please enter a valid email address");
+  } else {
+    hideFieldError("email");
+  }
+});
+
+passwordInput.addEventListener("blur", () => {
+  if (!passwordInput.value) {
+    showFieldError("password", "This field is required");
+  } else {
+    hideFieldError("password");
+  }
+});
+
+passwordInput.addEventListener("focus", () => {
+  if (fieldErrVisible.password) hideFieldError("password");
+});
+
+passwordInput.addEventListener("input", () => {
+  if (fieldErrVisible.password) hideFieldError("password");
+
+  if (fieldErrVisible.network) hideFieldError("network");
+});
+
+submitBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  let hasError = false;
+
+  if (!emailInput.value || !isEmailAddressValid(emailInput.value)) {
+    showFieldError("email", "Please enter a valid email address");
+    hasError = true;
+  }
+
+  if (!passwordInput.value) {
+    showFieldError("password", "This field is required");
+    hasError = true;
+  }
+
+  if (hasError) return;
+
+  try {
+    const res = await fetch(`/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json",
+      },
+      body: JSON.stringify({
+        email: emailInput.value,
+        password: passwordInput.value,
+      }),
+    });
+    const data = await res.json();
+
+    if (res.ok && data.success) {
+      window.location.href = data.redirectUrl;
+    } else if (!res.ok) {
+      showFieldError("network", data.error);
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    showFieldError("network", "Something went wrong");
+  }
+});
