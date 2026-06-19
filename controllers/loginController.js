fbed28a6f7a22163f377a6dad8a505513bdb8541
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-19 17:50:57 +0100

    feat: create user login authenticator middleware
    Add middleware that authenticates user and communicates with frontend
    using json api formats

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,5 +1,33 @@
+import passport from "passport";
+
 function loginPage(req, res) {
   res.render("login");
 }
 
-export { loginPage };
+function userLoginAuth(req, res, next) {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({ error: "Email and password are required" });
+  }
+
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: info.message || "Email or password is incorrect" });
+    }
+
+    req.logIn(user, (error) => {
+      if (error) {
+        return next(error);
+      }
+
+      return res.status(200).json({ success: true, redirectUrl: "/home" });
+    });
+  })(req, res, next);
+}
+
+export { loginPage, userLoginAuth };
