7f0132698a46bf00713926a452c0bf955a3a25cb
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-20 02:27:57 +0100

    refactor: remove redundant local value duplication
    Stop passing user as a local object to home render page since it has
    been made redundant due to previous middleware that handled that before
    route definitions

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,7 +3,7 @@ import { getAllPosts } from "../db/queries.js";
 async function homeGet(req, res, next) {
   try {
     const posts = await getAllPosts();
-    res.render("home", { posts, user: req.user });
+    res.render("home", { posts });
   } catch (error) {
     next(error);
   }
