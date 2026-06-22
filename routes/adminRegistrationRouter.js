2f1370c6dacd415e8f36828ce8bc177ecd158ad7
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-22 19:36:17 +0100

    feat: add requireMember guard to admin reg route

--- routes/adminRegistrationRouter.js
+++ routes/adminRegistrationRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { adminRegistrationGet } from "../controllers/adminRegistrationController.js";
+import { requireMember } from "../middleware/authGuards.js";
 
 const adminRegistrationRouter = Router();
 
-adminRegistrationRouter.get("/", adminRegistrationGet);
+adminRegistrationRouter.get("/", requireMember, adminRegistrationGet);
 
 export { adminRegistrationRouter };
