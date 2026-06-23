5937a49ad7452d9e2624af4f068cc26c2da4026b
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-23 04:15:16 +0100

    feat: add redirectIfAuthenticated auth guard
    Ensure user is redirected if authenticated and visiting the login or
    register pages via get or post requests

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -3,15 +3,17 @@ import { loginPage, userLoginAuth } from "../controllers/loginController.js";
 import passport from "passport";
 import { loginValidationRules } from "../validators/loginValidation.js";
 import { handleValidationErrors } from "../middleware/validationHandlers.js";
+import { redirectIfAuthenticated } from "../middleware/authGuards.js";
 
 const loginRouter = Router();
 
 loginRouter.post(
   "/",
+  redirectIfAuthenticated,
   loginValidationRules,
   handleValidationErrors,
   userLoginAuth
 );
-loginRouter.get("/", loginPage);
+loginRouter.get("/", redirectIfAuthenticated, loginPage);
 
 export { loginRouter };
