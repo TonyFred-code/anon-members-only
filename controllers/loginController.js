55c1281ec7542b699253ee54a735d8a58a89bcdb
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-06-23 02:13:57 +0100

    refactor: add login form validation rules
    Setup validation rules using express validator to ensure no empty field
    is sent neither is a malformed email sent to the server. This ensures
    userLoginAuth doesn't need to check for missing fields in its
    implementation as handleValidationErrors breaks the request if error is
    found by express validator

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,16 +5,6 @@ function loginPage(req, res) {
 }
 
 function userLoginAuth(req, res, next) {
-  const missingFields = [];
-  if (!req.body.email)
-    missingFields.push({ path: "email", msg: "Email is required" });
-  if (!req.body.password)
-    missingFields.push({ path: "password", msg: "Password is required" });
-
-  if (missingFields.length > 0) {
-    return res.status(400).json({ errors: missingFields });
-  }
-
   passport.authenticate("local", (err, user, info) => {
     if (err) {
       return next(err);
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { loginPage, userLoginAuth } from "../controllers/loginController.js";
 import passport from "passport";
+import { loginValidationRules } from "../validators/loginValidation.js";
+import { handleValidationErrors } from "../middleware/validationHandlers.js";
 
 const loginRouter = Router();
 
-loginRouter.post("/", userLoginAuth);
+loginRouter.post(
+  "/",
+  loginValidationRules,
+  handleValidationErrors,
+  userLoginAuth
+);
 loginRouter.get("/", loginPage);
 
 export { loginRouter };
